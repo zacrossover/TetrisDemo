25f0239f46e26925a20a55d8c41f73e50aaa9923
王震 <1320550505@qq.com>
2023-06-10 23:27:22 +0800

    一、添加排行榜

---
+++ TetrisDemo/list.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TetrisDemo
+{
+    partial class list
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lvRanking = new ListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lvRanking
+            // 
+            lvRanking.ItemHeight = 24;
+            lvRanking.Location = new Point(32, 60);
+            lvRanking.Name = "lvRanking";
+            lvRanking.Size = new Size(487, 484);
+            lvRanking.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(202, 24);
+            label1.TabIndex = 1;
+            label1.Text = "姓名                        得分";
+            label1.Click += label1_Click;
+            // 
+            // list
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(552, 562);
+            Controls.Add(label1);
+            Controls.Add(lvRanking);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "list";
+            Text = "排行榜";
+            Load += list_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lvRanking;
+        private Label label1;
+    }
+}
---
+++ TetrisDemo/list.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+namespace TetrisDemo
+{
+    public partial class list : Form
+    {
+        private const string RankingListFilePath = "RankingList.txt"; // 存储排行榜数据的文件路径
+
+        private List<RankingItem> _rankings; // 排行榜数据列表
+
+        public list()
+        {
+            InitializeComponent();
+            LoadRankingList(); // 加载排行榜数据
+        }
+
+        // 加载排行榜数据
+        private void LoadRankingList()
+        {
+            _rankings = new List<RankingItem>();
+            if (File.Exists(RankingListFilePath))
+            {
+                // 从文本文件中读取排行榜数据
+                var lines = File.ReadAllLines(RankingListFilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(' ');
+                    if (parts.Length >= 2 && int.TryParse(parts[1], out int score))
+                    {
+                        _rankings.Add(new RankingItem(parts[0], score));
+                    }
+                }
+            }
+            _rankings = _rankings.OrderByDescending(r => r.Score).ToList(); // 按积分从高到低排序
+            UpdateUI(); // 更新UI显示
+        }
+
+        // 更新UI显示
+        private void UpdateUI()
+        {
+            lvRanking.Items.Clear();
+            foreach (var rankingItem in _rankings)
+            {
+                lvRanking.Items.Add(rankingItem.Name + "                        " + rankingItem.Score.ToString());
+
+            }
+        }
+
+        // 添加排行榜项
+        public void AddRanking(string name, int score)
+        {
+            _rankings.Add(new RankingItem(name, score));
+            _rankings = _rankings.OrderByDescending(r => r.Score).ToList(); // 按积分从高到低排序
+            UpdateUI(); // 更新UI显示
+
+            // 将排行榜数据写入文本文件
+            var lines = _rankings.Select(r => $"{r.Name}\t{r.Score}");
+            File.WriteAllLines(RankingListFilePath, lines);
+        }
+
+        private void list_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 排行榜项类
+        private class RankingItem
+        {
+            public string Name { get; }
+            public int Score { get; }
+
+            public RankingItem(string name, int score)
+            {
+                Name = name;
+                Score = score;
+            }
+        }
+
+        private void lvRanking_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvRanking_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
